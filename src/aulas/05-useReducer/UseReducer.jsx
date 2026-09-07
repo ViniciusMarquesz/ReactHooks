1b72c034a9b5fcafc763b5ec0914f4df8cc09f76
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-09-07 15:19:59 -0300

    UseContext

--- src/aulas/05-useReducer/UseReducer.jsx
+++
@@ -1,9 +0,0 @@
-function UseReducer() {
-  return (
-    <div>
-      {" "}
-      <h1>Aula: useReducer</h1> <p>Minha primeira aula de React Hooks.</p>{" "}
-    </div>
-  );
-}
-export default UseReducer;
