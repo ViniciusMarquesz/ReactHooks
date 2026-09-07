553fa7d867145776143b3b08d8a3041c52717983
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-09-07 15:26:34 -0300

    Reducers

--- src/aulas/05-useReducer2/UseReducer2.jsx
+++ src/aulas/05-useReducer2/UseReducer2.jsx
@@ -1,41 +1,56 @@
 import { useReducer, useState } from "react";
 
-
 const reducer = (state, action) => {
   switch (action.type) {
     case "add-task":
       return {
         tasks: [
-          ...state.tasks, {name: action.payload, isCompleted: false},
+          ...state.tasks,
+          {
+            name: action.payload,
+            isCompleted: false,
+          },
         ],
       };
+
     default:
       return state;
   }
 };
 
 const AppUseContext2 = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    tasks: [],
+  });
 
-  const [state, dispatch] = useReducer(reducer, { counter: 0});
   const [inputValue, setInputValue] = useState("");
 
-
   return (
-   <div>
-    <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-    <button>
-      onclick={() => {
-        dispatch({ type: "add-task", payload: inputValue });
-        setInputValue("");
-      }}
-      Adicionar
-    </button>
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
 
-    {state.tasks.map(task => <p key={task.name}>{task.name}</p>)}
-   </div>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "add-task",
+            payload: inputValue,
+          });
 
+          setInputValue("");
+        }}
+      >
+        Adicionar
+      </button>
+
+      {state.tasks.map((task) => (
+        <p key={task.name}>{task.name}</p>
+      ))}
+    </div>
   );
-
 };
 
 export default AppUseContext2;
