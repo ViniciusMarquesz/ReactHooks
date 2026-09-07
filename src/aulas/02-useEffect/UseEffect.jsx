45403157b80b80e9d64a36400bd86f9c6a9ea4f4
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-09-07 12:17:34 -0300

    UseEffect

--- src/aulas/02-useEffect/UseEffect.jsx
+++ src/aulas/02-useEffect/UseEffect.jsx
@@ -1,9 +1,40 @@
-function UseEffect() {
+import { useState, useEffect } from "react"
+
+const AppEffect  = () => {
+
+  const [resourceType, setResourceType] = useState("Posts");
+
+  const changeResourceType = (resourceType) => {
+    setResourceType(resourceType);
+  }
+
+  useEffect(() => {
+    const fetchResourceTypes = async () => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/${resourceType}`);
+
+      const data = await response.json();
+      console.log(data)
+    }
+
+    fetchResourceTypes();
+
+  }, [resourceType]);
+
+
+
+
   return (
     <div>
-      {" "}
-      <h1>Aula: useEffect</h1> <p>Minha primeira aula de React Hooks.</p>{" "}
+      <h1>{resourceType}</h1>
+      <div style={{display: "flex", alignItems: "center"}}>
+        <button onClick={() => changeResourceType("Posts")}>Posts</button>
+        <button onClick={() => changeResourceType("Comments")}>Comments</button>
+        <button onClick={() => changeResourceType("Todos")}>Todos</button>
+      </div>
     </div>
+
   );
-}
-export default UseEffect;
+
+};
+
+export default AppEffect;
